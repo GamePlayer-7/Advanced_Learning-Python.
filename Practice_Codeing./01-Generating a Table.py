3108054e56628ea6b60d1e6ef59636146e23e183
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2023-01-05 11:20:41 +0530

    Create 01-Generating a Table.py

---
+++ Practice_Codeing./01-Generating a Table.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+# Below is the Output :
+
+   calories  duration
+0       420        50
+1       380        40
+2       390        45
