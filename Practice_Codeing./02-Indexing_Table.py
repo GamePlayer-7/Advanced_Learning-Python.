1087f77bd793cd27b5c9b61da0e895c3ff74f528
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2023-01-06 11:10:30 +0530

    Create 02-Indexing_Table.py

---
+++ Practice_Codeing./02-Indexing_Table.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df)
+
+
+# Below is the Output :
+
+      calories  duration
+day1       420        50
+day2       380        40
+day3       390        45
