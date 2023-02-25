3d2f63bd3ba50d9e9ef08522bab31997a3cf7aee
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2023-02-25 11:31:28 +0530

    Create README.cmd

---
+++ Matplotlib/README.cmd
@@ -0,0 +1,7 @@
+Matplotlib :
+ 
+* Matplotlib is a plotting library for the Python programming language.
+
+* Its numerical mathematics extension of NumPy. 
+
+* It provides an object-oriented API for embedding plots into applications using general-purpose GUI toolkits like Tkinter, wxPython, Qt, or GTK.
